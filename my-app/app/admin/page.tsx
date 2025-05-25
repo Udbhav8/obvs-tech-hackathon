5a371a5ccfa5f1cc7c93b083c38c554ef9e5d032
Tanuj Dargan <dargantanuj@gmail.com>
2025-05-25 15:26:13 -0700

    Fix react key warning

--- my-app/app/admin/page.tsx
+++ my-app/app/admin/page.tsx
@@ -176,12 +176,12 @@ export default function AdminDashboard() {
             <div className="p-4">
               {/* <h4 className="mb-4 text-sm font-medium leading-none">Tags</h4> */}
               {tags.map((tag) => (
-                <>
-                  <div key={tag} className="text-m">
+                <React.Fragment key={tag}>
+                  <div className="text-m">
                     {tag}
                   </div>
                   <Separator className="mt-2 mb-2"/>
-                </>
+                </React.Fragment>
               ))}
             </div>
           </ScrollArea>
